ad4e4eac4554cb6740790246b0d14262aefddef5
clem3109 <dev@radenac.me>
2025-08-26 18:02:47 +0200

    Remove 404 error page link for now

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,6 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html', // todo
 			precompress: false,
 			strict: true
 		}),
