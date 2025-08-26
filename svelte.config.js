2e7268850329751e46797917ba8365d09fc2fdba
clem3109 <dev@radenac.me>
2025-08-26 18:08:46 +0200

    try to correct with the {base}/

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
+			fallback: '404.html', // todo
 			precompress: false,
 			strict: true
 		}),
