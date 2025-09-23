ace54d2835f1ff036194459f741b8739edd5508b
clem3109 <dev@radenac.me>
2025-09-23 02:28:26 +0200

    Character and layout test

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,12 +1,12 @@
 <nav style="grid-area: nav;">
 	<ul>
-		<li><a href="#" class="bg-blue">Product</a></li>
-		<li><a href="/malnutrition" class="bg-red">Malnutrion</a></li>
 		<li class="inline-block w-[10em]">
-			<a href="/" aria-label="Projet">
+			<a href="/" aria-label="Projet" style="box-shadow: none !important">
 				<enhanced:img src="$lib/assets/logo-simple.svg" alt="Wooby Logo" class="w-full h-full"/>
 			</a>
 		</li> 
+		<li><a href="#" class="bg-blue">Product</a></li>
+		<li><a href="/malnutrition" class="bg-red">Malnutrion</a></li>
 		<li><a href="#" class="bg-yellow">Humanitarian aid</a></li>
 		<li><a href="/who-we-are" class="bg-purple">Who we are</a></li>
 	</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,47 @@
 <header class="w-full">
-	<enhanced:img src="$lib/assets/main-thumbnail.svg" alt="Wooby Logo with decorations" class="block mx-auto w-[90%] max-w-5xl my-15"/>
+	<enhanced:img src="$lib/assets/characters/three.svg" alt="Wooby Logo with decorations" class="block mx-auto w-[60%] max-w-5xl my-15"/>
 </header>
-<h1>All you need to know about Wooby!</h1>
-<p>Wooby was born at the Airbus Humanity Lab as one of its first initiatives. The project emerged from a call for proposals by the Airbus Foundation, inviting NGOs working with them to present challenges where technology innovation could make a meaningful difference.</p>
-<p>MEDAIR, one of the key partners in this call, accepted the challenge and presented the pressing issue of child malnutrition. They highlighted the difficulty in monitoring infant and young child weight in field conditions, especially in resource-limited environments, and proposed that this problem could be solved through technology.</p>
-<p>In 2019, the first group of volunteers came together to bring Wooby to life, beginning work on an initial prototype at the Airbus ProtoSpace in Toulouse, France. This initial team focused on validating the feasibility of achieving high-precision weight measurements at a fraction of the cost of commercial alternatives. From the very beginning, Wooby was designed as an open-source project, leveraging the power of collaboration and shared knowledge to maximize its impact worldwide. By making its design and methodology accessible to anyone, the project enables communities, NGOs, and healthcare professionals to build and deploy their own Wooby scales, adapting them to local needs.</p>
-<p>Over time, the project evolved through iterative prototyping and testing, refining both the technical design and its adaptability to real-world conditions. Wooby stands as a testament to the potential of open innovation and teamwork in tackling global health challenges through accessible and sustainable technology.</p>
+<button class="block mx-auto my-15 text-4xl">Contact us</button>
+<section class="stripe py-5">
+	<h1>All you need to know about Wooby!</h1>
+	<p>Wooby was born at the Airbus Humanity Lab as one of its first initiatives. The project emerged from a call for proposals by the Airbus Foundation, inviting NGOs working with them to present challenges where technology innovation could make a meaningful difference.</p>
+	<p>MEDAIR, one of the key partners in this call, accepted the challenge and presented the pressing issue of child malnutrition. They highlighted the difficulty in monitoring infant and young child weight in field conditions, especially in resource-limited environments, and proposed that this problem could be solved through technology.</p>
+	<p>In 2019, the first group of volunteers came together to bring Wooby to life, beginning work on an initial prototype at the Airbus ProtoSpace in Toulouse, France. This initial team focused on validating the feasibility of achieving high-precision weight measurements at a fraction of the cost of commercial alternatives. From the very beginning, Wooby was designed as an open-source project, leveraging the power of collaboration and shared knowledge to maximize its impact worldwide. By making its design and methodology accessible to anyone, the project enables communities, NGOs, and healthcare professionals to build and deploy their own Wooby scales, adapting them to local needs.</p>
+	<p>Over time, the project evolved through iterative prototyping and testing, refining both the technical design and its adaptability to real-world conditions. Wooby stands as a testament to the potential of open innovation and teamwork in tackling global health challenges through accessible and sustainable technology.</p>
+</section>
+<section class="py-5">
+	<h1>Our Mission</h1>
+	<div class="md:columns-3">
+		<div class="block mx-auto">
+			<h3>Educational Play</h3>
+			<span>
+				Learn more and more !
+			</span>
+		</div>
+		<div class="block mx-auto">
+			<h3>Educational Play</h3>
+			<span>
+				Learn more and more !
+			</span>
+		</div>
+		<div class="block mx-auto">
+			<h3>Educational Play</h3>
+			<span>
+				Learn more and more !
+			</span>
+		</div>
+	</div>
+</section>
+<section class="stripe py-5" style="--bg-color: var(--bg-yellow)">
+	<h1>The stories</h1>
+	<div class="md:columns-2">
+		<div class="bg-white rounded-xl p-3 mb-4">
+			<img class="w-full aspect-video" /> 
+			<h3>Columbia</h3>
+		</div>
+		<div class="bg-white rounded-xl p-3 mb-4">
+			<img class="w-full aspect-video" /> 
+			<h3>South Soudan</h3>
+		</div>
+	</div>
+</section>
