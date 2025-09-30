448767b47815fffffa2b7121c76eb24a7b8ed826
clem3109 <dev@radenac.me>
2025-09-30 15:15:48 +0200

    Footer v1

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,3 +1,9 @@
+<script>
+	import Fa from 'svelte-fa'
+	import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+	import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+</script>
+
 <div id="color-bar" style="grid-area: pre-footer;">
 	<div class="w-1/5 bg-blue"></div>
 	<div class="w-1/5 bg-red"></div>
@@ -5,10 +11,29 @@
 	<div class="w-1/5 bg-purple"></div>
 	<div class="w-1/5 bg-green"></div>
 </div>
-<footer class="container mt-6" style="grid-area: footer;">
-
-	Footer todo
-	<!--<p>Il faudra un truc ici.</p>
-		
-	Merci Google fonts, tailwind, Font Awesome-->
+<footer class="container" style="grid-area: footer;">
+	<!--	Merci Google fonts, tailwind, Font Awesome-->
+	<div class="flex gap-15">
+		<enhanced:img src="$lib/assets/logo-simple.svg" alt="Wooby Logo" style="width: clamp(10rem, 18vw, 25rem);"/>
+		<div class="basis-full">
+			<h3>Follow us</h3>
+			<ul class="social flex gap-5 w-full [&_a]:p-2 [&_a]:text-3xl">
+				<li><a href="https://www.linkedin.com/company/wooby/" class="bg-blue" title="Wooby's LinkedIn"><Fa icon={faLinkedinIn} /></a></li>
+				<li><a href="https://www.instagram.com/woobyproject/" class="bg-purple" title="Wooby's Instagram"><Fa icon={faInstagram} /></a></li>
+			</ul>
+		</div>
+		<div class="basis-full">
+			<h3>Contact us</h3>
+			<p>
+				<a href="mailto:hello@wooby.org">hello@wooby.org</a>
+			</p>
+		</div>
+		<div class="basis-full">
+			<h3>Partners</h3>
+			<ul>
+				<li><a href="https://discoveryspace-foundation.airbus.com/">Airbus Fondation</a></li>
+			</ul>
+		</div>
+	</div>
+	<p class="mt-4 text-sm text-center"><a href="/terms-and-conditions">Terms & Conditions</a> | © 2024 Wooby. All Rights Reserved.</p>
 </footer>
---
+++ src/routes/terms-and-conditions/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Terms & Conditions</h1>
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque animi tempore dolorem, cumque est voluptatem. Rem, quod laborum dolore sunt corporis delectus porro exercitationem minus, cum cupiditate ipsam sapiente dignissimos?
+</p>
